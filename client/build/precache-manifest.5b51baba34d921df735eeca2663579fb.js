58e6884c92aba805495f1769a2175f68c1825965
jaimepreuss <jaimepreuss@gmail.com>
2020-08-18 21:40:37 -0700

    react app with books!

---
+++ client/build/precache-manifest.5b51baba34d921df735eeca2663579fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7faa1bd39c086e26110",
+    "url": "/static/css/main.6e7d59cf.chunk.css"
+  },
+  {
+    "revision": "b7faa1bd39c086e26110",
+    "url": "/static/js/main.5f0ce53a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "50510bad2c5c3f30be83",
+    "url": "/static/js/2.05fd254b.chunk.js"
+  },
+  {
+    "revision": "832494fddcf174e72bb3c05e6461021c",
+    "url": "/index.html"
+  }
+];
